5abe07d90b0b6414a98c10aff78c225dff28e148
Wes Carr <wescarr@gmail.com>
2020-03-22 10:09:49 -0700

    Add spacing for settings view

--- lib/settings.dart
+++ lib/settings.dart
@@ -26,38 +26,40 @@ class SettingsState extends State<Settings> {
                 onPressed: () {
                   Navigator.pop(context);
                 })),
-        body: Column(children: [
-          DropdownButton(
-              value: _age,
-              onChanged: (value) => setState(() => _age = value),
-              hint: Text('Age'),
-              isExpanded: true,
-              items: [
-                '< 2 years',
-                '2 - 4',
-                '5 - 9',
-                '10 - 18',
-                '19 - 29',
-                '30 - 39',
-                '40 - 49',
-                '50 - 59',
-                '60 - 69',
-                '70 - 79',
-                '80+'
-              ]
-                  .map((label) =>
-                      DropdownMenuItem(value: label, child: Text(label)))
-                  .toList()),
-          SizedBox(height: 10),
-          DropdownButton(
-              value: _gender,
-              onChanged: (value) => setState(() => _gender = value),
-              hint: Text('Gender'),
-              isExpanded: true,
-              items: ['Female', 'Male', 'Other']
-                  .map((label) =>
-                      DropdownMenuItem(value: label, child: Text(label)))
-                  .toList()),
-        ]));
+        body: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(children: [
+              DropdownButton(
+                  value: _age,
+                  onChanged: (value) => setState(() => _age = value),
+                  hint: Text('Age'),
+                  isExpanded: true,
+                  items: [
+                    '< 2 years',
+                    '2 - 4',
+                    '5 - 9',
+                    '10 - 18',
+                    '19 - 29',
+                    '30 - 39',
+                    '40 - 49',
+                    '50 - 59',
+                    '60 - 69',
+                    '70 - 79',
+                    '80+'
+                  ]
+                      .map((label) =>
+                          DropdownMenuItem(value: label, child: Text(label)))
+                      .toList()),
+              SizedBox(height: 20),
+              DropdownButton(
+                  value: _gender,
+                  onChanged: (value) => setState(() => _gender = value),
+                  hint: Text('Gender'),
+                  isExpanded: true,
+                  items: ['Female', 'Male', 'Other']
+                      .map((label) =>
+                          DropdownMenuItem(value: label, child: Text(label)))
+                      .toList()),
+            ])));
   }
 }
