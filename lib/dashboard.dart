67228b8f054b0b4ff4dfa4862871a0bd5f22b664
Wes Carr <wescarr@gmail.com>
2020-03-25 14:41:57 -0700

    Pull to refresh to check for exposures. Add no exposures view

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'helper/check_exposures.dart';
 import 'storage/location.dart';
 
 class Dashboard extends StatefulWidget {
@@ -11,10 +12,12 @@ class Dashboard extends StatefulWidget {
 }
 
 class DashboardState extends State {
+  var _recentExposure;
   Completer<GoogleMapController> _mapController = Completer();
 
   void initState() {
     super.initState();
+    _recentExposure = loadExposures();
   }
 
   Future<LocationModel> loadExposures() async {
@@ -23,115 +26,170 @@ class DashboardState extends State {
 
     var locations = await LocationModel.findAll(
         limit: 1,
-        where: 'DATE(timestamp) > DATE(\'$timestamp\')',
+        where: 'DATE(timestamp) > DATE(?) and exposure = 1',
+        whereArgs: [timestamp],
         orderBy: 'timestamp DESC');
 
     return locations.isEmpty ? null : locations.first;
   }
 
+  Future<void> refreshExposures() async {
+    var found = await checkExposures();
+    if (!found) {
+      return;
+    }
+
+    var load = loadExposures();
+    setState(() {
+      _recentExposure = load;
+      return;
+    });
+
+    var location = await load;
+    if (location != null) {
+      var controller = await _mapController.future;
+      controller.animateCamera(CameraUpdate.newLatLng(
+          LatLng(location.latitude, location.longitude)));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    var loc = LatLng(47.6513435, -122.3511888);
     var subhead = Theme.of(context).textTheme.subhead;
     var title = subhead.merge(TextStyle(fontWeight: FontWeight.bold));
     var body = Theme.of(context).textTheme.body1.merge(TextStyle(height: 1.3));
     var alertText = TextStyle(color: Colors.white);
 
     return FutureBuilder(
-        future: loadExposures(),
+        future: _recentExposure,
         builder: (context, AsyncSnapshot<LocationModel> snapshot) {
           if (!snapshot.hasData) {
-            return Container();
+            return Padding(
+                padding: EdgeInsets.all(15),
+                child: RefreshIndicator(
+                    onRefresh: refreshExposures,
+                    child: ListView(children: [
+                      Container(
+                          decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                              padding: EdgeInsets.all(15),
+                              child: Row(children: [
+                                Expanded(
+                                    child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                      Text('No Exposures Found',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .title
+                                              .merge(alertText)),
+                                      Text('In the last 7 days',
+                                          style: alertText)
+                                    ])),
+                                Image.asset('assets/people_arrows_icon.png',
+                                    height: 40),
+                              ])))
+                    ])));
           }
 
           var location = snapshot.data;
           if (location != null) {
             var timestamp = location.timestamp.toLocal();
+            var loc = LatLng(location.latitude, location.longitude);
 
             return Padding(
                 padding: EdgeInsets.all(15),
-                child: ListView(children: [
-                  Container(
-                      decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Padding(
-                          padding: EdgeInsets.all(15),
-                          child: Row(children: [
-                            Expanded(
-                                child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                  Text('Possible Exposure',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .title
-                                          .merge(alertText)),
-                                  Text('In the last 7 days', style: alertText)
-                                ])),
-                            Image.asset('assets/shield_virus_icon.png',
-                                height: 40),
-                          ]))),
-                  SizedBox(height: 10),
-                  Card(
-                      child: Column(children: [
-                    SizedBox(
-                        height: 150,
-                        child: GoogleMap(
-                          mapType: MapType.normal,
-                          myLocationEnabled: false,
-                          myLocationButtonEnabled: false,
-                          initialCameraPosition:
-                              CameraPosition(target: loc, zoom: 16),
-                          minMaxZoomPreference: MinMaxZoomPreference(10, 18),
-                          markers: [
-                            Marker(markerId: MarkerId('1'), position: loc)
-                          ].toSet(),
-                          onMapCreated: (controller) {
-                            if (!_mapController.isCompleted) {
-                              _mapController.complete(controller);
-                            }
-                          },
-                        )),
-                    ListTile(
-                      isThreeLine: true,
-                      title: Text(
-                          '${DateFormat.Md().format(timestamp)} ${DateFormat('ha').format(timestamp).toLowerCase()} - ${DateFormat('ha').format(timestamp.add(Duration(hours: 1))).toLowerCase()}'),
-                      subtitle: Text(
-                          'Your location overlapped with someone who reported as having COVID-19.'),
-                    ),
-                  ])),
-                  SizedBox(height: 20),
-                  Column(children: [
-                    Text('HAVE A PLAN FOR IF YOU GET SICK', style: subhead),
-                    SizedBox(height: 20),
-                    Text('Consult with your healthcare provider', style: title),
-                    Text(
-                        'for more information about monitoring your health for symptoms suggestive of COVID-19',
-                        style: body,
-                        textAlign: TextAlign.center),
-                    SizedBox(height: 20),
-                    Text('Stay in touch with others by phone or email',
-                        style: title),
-                    Text(
-                        'You may need to ask for help from friends, family, neighbors, community health workers, etc. if you become sick.',
-                        style: body,
-                        textAlign: TextAlign.center),
-                    SizedBox(height: 20),
-                    Text('Determine who can care for you', style: title),
-                    Text('if your caregiver gets sick',
-                        style: body, textAlign: TextAlign.center),
-                    FlatButton(
-                      child: Text('FIND OUT MORE',
-                          style:
-                              TextStyle(decoration: TextDecoration.underline)),
-                      onPressed: () => launch(
-                          'https://www.cdc.gov/coronavirus/2019-ncov/specific-groups/get-ready.html'),
-                    )
-                  ]),
-                  SizedBox(height: 100), // Account for floating action button
-                ]));
+                child: RefreshIndicator(
+                    onRefresh: refreshExposures,
+                    child: ListView(children: [
+                      Container(
+                          decoration: BoxDecoration(
+                              color: Colors.orange,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                              padding: EdgeInsets.all(15),
+                              child: Row(children: [
+                                Expanded(
+                                    child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                      Text('Possible Exposure',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .title
+                                              .merge(alertText)),
+                                      Text('In the last 7 days',
+                                          style: alertText)
+                                    ])),
+                                Image.asset('assets/shield_virus_icon.png',
+                                    height: 40),
+                              ]))),
+                      SizedBox(height: 10),
+                      Card(
+                          child: Column(children: [
+                        SizedBox(
+                            height: 150,
+                            child: GoogleMap(
+                              mapType: MapType.normal,
+                              myLocationEnabled: false,
+                              myLocationButtonEnabled: false,
+                              initialCameraPosition:
+                                  CameraPosition(target: loc, zoom: 16),
+                              minMaxZoomPreference:
+                                  MinMaxZoomPreference(10, 18),
+                              markers: [
+                                Marker(markerId: MarkerId('1'), position: loc)
+                              ].toSet(),
+                              onMapCreated: (controller) {
+                                if (!_mapController.isCompleted) {
+                                  _mapController.complete(controller);
+                                }
+                              },
+                            )),
+                        ListTile(
+                          isThreeLine: true,
+                          title: Text(
+                              '${DateFormat.Md().format(timestamp)} ${DateFormat('ha').format(timestamp).toLowerCase()} - ${DateFormat('ha').format(timestamp.add(Duration(hours: 1))).toLowerCase()}'),
+                          subtitle: Text(
+                              'Your location overlapped with someone who reported as having COVID-19.'),
+                        ),
+                      ])),
+                      SizedBox(height: 20),
+                      Column(children: [
+                        Text('HAVE A PLAN FOR IF YOU GET SICK', style: subhead),
+                        SizedBox(height: 20),
+                        Text('Consult with your healthcare provider',
+                            style: title),
+                        Text(
+                            'for more information about monitoring your health for symptoms suggestive of COVID-19',
+                            style: body,
+                            textAlign: TextAlign.center),
+                        SizedBox(height: 20),
+                        Text('Stay in touch with others by phone or email',
+                            style: title),
+                        Text(
+                            'You may need to ask for help from friends, family, neighbors, community health workers, etc. if you become sick.',
+                            style: body,
+                            textAlign: TextAlign.center),
+                        SizedBox(height: 20),
+                        Text('Determine who can care for you', style: title),
+                        Text('if your caregiver gets sick',
+                            style: body, textAlign: TextAlign.center),
+                        FlatButton(
+                          child: Text('FIND OUT MORE',
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline)),
+                          onPressed: () => launch(
+                              'https://www.cdc.gov/coronavirus/2019-ncov/specific-groups/get-ready.html'),
+                        )
+                      ]),
+                      SizedBox(
+                          height: 100), // Account for floating action button
+                    ])));
           }
 
           return Container();
