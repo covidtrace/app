c892900c022e8de3706a171d5f0cdec44d10e41f
Josh Gummersall <josh@standardlabs.dev>
2020-04-03 09:57:42 -0700

    Use sqflite migration tool

--- lib/storage/db.dart
+++ lib/storage/db.dart
@@ -1,34 +1,65 @@
 import 'dart:async';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_migration/sqflite_migration.dart';
 import 'package:uuid/uuid.dart';
+
+final uuid = Uuid().v4();
+
+// This value should essentially never change. Any changes to the DB schema
+// must be applied via the `migrationScripts` list.
+List<String> initialScript = [
+  '''
+CREATE TABLE location (
+  id INTEGER PRIMARY KEY,
+  longitude REAL,
+  latitude REAL,
+  cell_id TEXT,
+  speed REAL,
+  activity TEXT,
+  sample INTEGER,
+  timestamp TEXT UNIQUE,
+  exposure INTEGER DEFAULT 0,
+  reported INTEGER DEFAULT 0
+ )
+  ''',
+  '''
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  uuid STRING,
+  track_location INTEGER,
+  longitude REAL,
+  latitude REAL,
+  home_radius REAL,
+  onboarding INTEGER,
+  last_check TEXT,
+  verify_token TEXT,
+  refresh_token TEXT
+)
+  ''',
+  "INSERT INTO user (uuid, home_radius, onboarding) VALUES ('$uuid', 40.0, 1)",
+  '''
+CREATE TABLE report (
+  id INTEGER PRIMARY KEY,
+  timestamp TEXT,
+  last_location_id INTEGER,
+  FOREIGN KEY (last_location_id) REFERENCES location (id) ON DELETE CASCADE
+)
+  ''',
+];
+
+List<String> migrationScripts = [];
 
 Future<String> _dataBasePath(String path) async {
   return join(await getDatabasesPath(), path);
 }
 
 Future<Database> _initDatabase() async {
-  var dbPath = await _dataBasePath('locations.db');
-  print(dbPath);
-
-  Future<Database> database =
-      openDatabase(dbPath, onCreate: (db, version) async {
-    // Set up location table
-    await db.execute(
-        "CREATE TABLE location(id INTEGER PRIMARY KEY, longitude REAL, latitude REAL, cell_id TEXT, speed REAL, activity TEXT, sample INTEGER, timestamp TEXT UNIQUE, exposure INTEGER DEFAULT 0, reported INTEGER DEFAULT 0)");
-
-    // Set up user table
-    await db.execute(
-        "CREATE TABLE user(id INTEGER PRIMARY KEY, uuid STRING, track_location INTEGER, gender STRING, age INTEGER, longitude REAL, latitude REAL, home_radius REAL, onboarding INTEGER, last_check TEXT, verify_token TEXT, refresh_token TEXT)");
-    await db.insert(
-        'user', {'uuid': Uuid().v4(), 'home_radius': 40.0, 'onboarding': 1});
-
-    // Set up reports table
-    await db.execute(
-        "CREATE TABLE report(id INTEGER PRIMARY KEY, timestamp TEXT, last_location_id INTEGER, FOREIGN KEY (last_location_id) REFERENCES location (id) ON DELETE CASCADE)");
-  }, version: 1);
-
-  return database;
+  return await openDatabaseWithMigration(
+      await _dataBasePath('locations.db'),
+      MigrationConfig(
+          initializationScript: initialScript,
+          migrationScripts: migrationScripts));
 }
 
 class Storage {
